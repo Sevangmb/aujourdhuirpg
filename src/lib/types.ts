4a856685540486007f41afea52a7de01ee2a2fd8
Sevan Safarian <sevangmb@gmail.com>
2025-06-30 17:33:45 +0000

    c est finu ?

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -11,4 +11,8 @@ export * from './types/geo-intelligence-types';
 export * from './types/finance-types';
 export * from './types/historical-contact-types';
 export * from './types/era-types';
+export * from './types/choice-types';
+export * from './types/physiology-types';
+export * from './types/recipe-types';
+export * from './types/book-types';
 export type { CharacterSummary, SaveSummary } from '@/services/firestore-service';
